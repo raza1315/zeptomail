77dc472d29ebcf7395fefbe0c470892e768cdbeb
raza1315 <razasfs2003@gmail.com>
2025-02-13 13:30:34 +0530

    iniitial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors=require("cors");
+
+const app=express();
+const PORT=3000;
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT,()=>{
+console.log()"server is running on port:",PORT);
+})
+
+app.post("/send-mail",(req,res)=>{
+try{}
+catch{
+
+}
+})
